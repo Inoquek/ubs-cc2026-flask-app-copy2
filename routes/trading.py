b326bbf56bab67c17929b7848267eaf0103214a6
rickbaishuak <rick.baishuak@gmail.com>
2025-09-06 16:32:38 +0800

    trading1

---
+++ routes/trading.py
@@ -0,0 +1,23 @@
+import os
+import json
+import logging
+from routes import app
+from flask import jsonify, request
+import math
+logger = logging.getLogger(__name__)
+
+class Sol:
+    def __init__(self, news):
+        self.news = news
+
+TEST_CASE = 0
+@app.route("/trading-bot", methods = ["POST"])
+def trading():
+    global TEST_CASE
+    data = request.get_json(silent=True) or {}
+    TEST_CASE += 1
+    logger.info(f"PROCESSING #{TEST_CASE}")
+
+    logging.warning(data)
+
+    return jsonify([])
