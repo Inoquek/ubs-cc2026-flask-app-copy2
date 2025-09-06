697d4e24c8f83667e37bc833a192f36ba9fd8832
rickbaishuak <rick.baishuak@gmail.com>
2025-09-06 13:24:24 +0800

    2

--- routes/diaries.py
+++ routes/diaries.py
@@ -10,7 +10,7 @@ class Sol:
         self.tasks = tasks # list of dicts
         self.edges = edges # list of dicts, process to become list of 3-tuples
         self.s0 = s0
-        self.INF = int(1e9)
+        self.INF = int(1e18)
     def process_graph(self):
         self.vertices = []
         for i, edge in enumerate(self.edges):
@@ -25,48 +25,75 @@ class Sol:
             self.edges[i] = (self.vertices.index(u), self.vertices.index(v), edge['fee'])
         self.n = len(self.vertices)
         self.calc_dists()
-    
-    def preprocess_tasks(self):
+
+    def solve(self):
+        self.process_graph()
+        
         for task in self.tasks:
             task['station'] = self.vertices.index(task['station'])
         self.s0 = self.vertices.index(self.s0)
 
-    def solve(self):
-        self.process_graph()
-        self.preprocess_tasks()
+        self.name_to_task = {}
         self.times = [0]
         for task in self.tasks:
+            self.name_to_task[task['name']] = task
             self.times.append(task['start'])
             self.times.append(task['end'])
         self.times = list(set(self.times))
         self.times.sort()
         # only storing ending times of tasks
-        self.dp = {(0, self.s0): (0, 0, [])} # (score, -fee, schedule)
-        answer = (0, 0, [])
+        self.dp = {(0, self.s0): (0, 0, None)} # (score, -fee, schedule)
+        answer = (0, 0, None)
+        self.par = {}
         for task in sorted(self.tasks, key = lambda task: task['end']):
             v_sit = task['station']
             t1, t2 = self.times.index(task['start']), self.times.index(task['end'])
             self.dp[(t2, v_sit)] = (-self.INF, 0, [])
-            for (t0, u), (score, fee, schedule) in self.dp.items():
+            for (t0, u), (score, fee, name) in self.dp.items():
                 if t0 > t1:
                     continue
                 # if we take t0:
-                score_t0, fee_t0, schedule_t0 = self.dp[(t0, u)]
-                score_t0 += task['score']
-                fee_t0 -= self.d[u][v_sit]
-                if (score_t0, fee_t0) >= self.dp[(t2, v_sit)][:-1]:
-                    self.dp[(t2, v_sit)] = (score_t0, fee_t0, schedule_t0 + [task['name']])
+                score += task['score']
+                fee -= self.d[u][v_sit]
+                if (score, fee) > self.dp[(t2, v_sit)][:-1]:
+                    self.dp[(t2, v_sit)] = (score, fee, task['name'])
+                    self.par[task['name']] = name
 
             score, fee, schedule = self.dp[(t2, v_sit)]
-            if_finish = (score, fee - self.d[v_sit][self.s0], schedule)
-            answer = max(answer, if_finish)
+            fee -= self.d[v_sit][self.s0]
+            if (score, fee) > answer[:-1]:
+                answer = (score, fee, task['name'])
         
+        print(answer)
+        last_task = answer[2]
+        schedule = []
+        fee_brute_force = 0
+        score_brute_force = 0
+        last_v = self.s0
+        while last_task is not None:
+            task_obj = self.name_to_task[last_task]
+            v = task_obj['station']
+            score_brute_force += task_obj['score']
+
+            fee_brute_force += self.d[last_v][v]
+            last_v = v
+            schedule.append(last_task)
+            last_task = self.par[last_task]
+        
+        fee_brute_force += self.d[last_v][self.s0]
+        if not (fee_brute_force == -answer[1]):
+            logger.error(f"fee incorrect: {fee_brute_force}, not {-answer[1]}")
+        if not (score_brute_force == answer[0]):
+            logger.error(f"fee incorrect: {score_brute_force}, not {answer[0]}")
+
+
+        schedule = schedule[::-1]
         answer = {
             'max_score': answer[0],
             "min_fee": -answer[1],
-            "schedule": answer[2]
+            "schedule": schedule
         }
-        logger.info(f"N is {self.n}, T is{len(self.times)}")
+        logger.info(self.n, len(self.times))
         return answer
 
 
@@ -82,27 +109,6 @@ class Sol:
                 for j in range(self.n):
                     self.d[i][j] = min(self.d[i][j], self.d[i][k] + self.d[k][j])
 
-# INPUT = {
-#   "tasks": [
-#     { "name": "A", "start": 480, "end": 540, "station": 1, "score": 2 },
-#     { "name": "B", "start": 600, "end": 660, "station": 2, "score": 1 },
-#     { "name": "C", "start": 720, "end": 780, "station": 3, "score": 3 },
-#     { "name": "D", "start": 840, "end": 900, "station": 4, "score": 1 },
-#     { "name": "E", "start": 960, "end": 1020, "station": 1, "score": 4 },
-#     { "name": "F", "start": 530, "end": 590, "station": 2, "score": 1 }
-#   ],
-#   "subway": [
-#     { "connection": [0, 1], "fee": 10 },
-#     { "connection": [1, 2], "fee": 10 },
-#     { "connection": [2, 3], "fee": 20 },
-#     { "connection": [3, 4], "fee": 30 }
-#   ],
-#   "starting_station": 0
-# }
-# tasks = INPUT['tasks']
-# edges = INPUT['subway']
-# s0 = INPUT['starting_station']
-# print(Sol(tasks, edges, s0).solve())
 
 @app.route("/princess-diaries", methods = ["POST"])
 def princess_diaries():
