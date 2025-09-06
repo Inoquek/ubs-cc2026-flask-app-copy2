42d2c6ac95995b6f2cb3fc528bd4e4d35f3cd49e
rickbaishuak <rick.baishuak@gmail.com>
2025-09-06 12:59:19 +0800

    1

--- routes/diaries.py
+++ routes/diaries.py
@@ -10,15 +10,18 @@ class Sol:
         self.tasks = tasks # list of dicts
         self.edges = edges # list of dicts, process to become list of 3-tuples
         self.s0 = s0
+        self.INF = int(1e9)
     def process_graph(self):
         self.vertices = []
         for i, edge in enumerate(self.edges):
             u, v = edge['connection']
-            if u not in self.vertices:
-                self.vertices.append(u)
-            if v not in self.vertices:
-                self.vertices.append(v)
+            self.vertices.append(u)
+            self.vertices.append(v)
             
+        self.vertices = list(set(self.vertices))
+        # print(f"vertices: {self.vertices}")
+        for i, edge in enumerate(self.edges):
+            u, v = edge['connection']
             self.edges[i] = (self.vertices.index(u), self.vertices.index(v), edge['fee'])
         self.n = len(self.vertices)
         self.calc_dists()
@@ -33,10 +36,9 @@ class Sol:
         self.preprocess_tasks()
         self.times = [0]
         for task in self.tasks:
-            if task['start'] not in self.times:
-                self.times.append(task['start'])
-            if task['end'] not in self.times:
-                self.times.append(task['end'])
+            self.times.append(task['start'])
+            self.times.append(task['end'])
+        self.times = list(set(self.times))
         self.times.sort()
         # only storing ending times of tasks
         self.dp = {(0, self.s0): (0, 0, [])} # (score, -fee, schedule)
@@ -44,14 +46,16 @@ class Sol:
         for task in sorted(self.tasks, key = lambda task: task['end']):
             v_sit = task['station']
             t1, t2 = self.times.index(task['start']), self.times.index(task['end'])
-            self.dp[(t2, v_sit)] = (-40_000, 0, [])
+            self.dp[(t2, v_sit)] = (-self.INF, 0, [])
             for (t0, u), (score, fee, schedule) in self.dp.items():
                 if t0 > t1:
                     continue
                 # if we take t0:
                 score_t0, fee_t0, schedule_t0 = self.dp[(t0, u)]
-                for_t0 = (score_t0 + task['score'], fee_t0 - self.d[u][v_sit], schedule_t0 + [task['name']])
-                self.dp[(t2, v_sit)] = max(self.dp[(t2, v_sit)], for_t0)
+                score_t0 += task['score']
+                fee_t0 -= self.d[u][v_sit]
+                if (score_t0, fee_t0) >= self.dp[(t2, v_sit)][:-1]:
+                    self.dp[(t2, v_sit)] = (score_t0, fee_t0, schedule_t0 + [task['name']])
 
             score, fee, schedule = self.dp[(t2, v_sit)]
             if_finish = (score, fee - self.d[v_sit][self.s0], schedule)
@@ -62,12 +66,12 @@ class Sol:
             "min_fee": -answer[1],
             "schedule": answer[2]
         }
+        logger.info(f"N is {self.n}, T is{len(self.times)}")
         return answer
 
 
     def calc_dists(self):
-        inf = int(1e9)
-        self.d = [[inf] * self.n for _ in range(self.n)]
+        self.d = [[self.INF] * self.n for _ in range(self.n)]
         for i in range(self.n):
             self.d[i][i] = 0
         for u, v, fee in self.edges:
@@ -77,7 +81,6 @@ class Sol:
             for i in range(self.n):
                 for j in range(self.n):
                     self.d[i][j] = min(self.d[i][j], self.d[i][k] + self.d[k][j])
-        logger.info(f"N is {self.n}")
 
 # INPUT = {
 #   "tasks": [
